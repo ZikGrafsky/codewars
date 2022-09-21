65629269940df7ac78840b5e3a0ffc760a0e5127
NAME <mail@gmail.com>
2022-09-21 06:33:16 +0300

    “continue

--- Persistent_Bugger/persistence.cpp
+++ Persistent_Bugger/persistence.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //решение с рекурсией
-int persistence(long long n){
+/*int persistence(long long n){
     int continue_value, returned_value;
     continue_value = 1;
     n > 9 ? returned_value = 1 : returned_value = 0;
@@ -15,11 +15,26 @@ int persistence(long long n){
     }
     (continue_value > 9) ? returned_value += persistence(continue_value) : returned_value = returned_value;
     return returned_value;
-}
+}*/
 //решение с циклами
-//int persistence(long long n){
-//
-//}
+int persistence(long long n){
+	int ret = 0;
+	int cont = 1;
+	std::cout << "n = " << n << std::endl; 
+	do{
+		n = cont;
+		cont = 1;
+		if (n > 9)
+			ret++;
+	    while (n > 0){
+		    cont *= n % 10;
+			n /= 10;
+			std::cout << "cont = " << cont << std::endl;
+		}
+		std::cout << "------------\n";
+	}while (cont > 0);
+	return ret;
+}
 
 int main(){
     cout << persistence(39) << "\n";
