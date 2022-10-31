1967d41f584c3faa36a81a6a89bdd23fd8b645fb
ZikGrafsky <silentzzephyr@gmail.com>
2022-10-31 07:22:33 +0300

    update

--- Average_Salary_Excluding_the_Minimum_and_Maximum_Salary/Solution.cpp
+++ Average_Salary_Excluding_the_Minimum_and_Maximum_Salary/Solution.cpp
@@ -7,13 +7,27 @@
 
 using namespace std;
 
-double average(vector<int>& salary) {
+double average_with_algorithm(vector<int>& salary) {
     double result = 0.0;
     for (auto it = salary.begin(); it != salary.end(); ++it) {
         if (it != max_element(salary.begin(), salary.end()) && it != min_element(salary.begin(), salary.end()))
             result += (double) *it;
     }
     return result / (salary.size() - 2);
+}
+
+double average(vector<int>& salary) {
+    double result = 0.0;
+    int max = 0;
+    int min = *salary.begin();
+    for (auto it = salary.begin(); it != salary.end(); ++it) {
+        if (*it > max)
+            max = *it;
+        if (*it < min)
+            min = *it;
+        result += (double) *it;
+    }
+    return ((result - (max + min)) / (salary.size() - 2));
 }
 
 int main(){
